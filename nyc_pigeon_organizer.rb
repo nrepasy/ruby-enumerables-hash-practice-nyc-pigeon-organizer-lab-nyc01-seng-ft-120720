36aa607f3820b8fa2e8ce2d28cde0beee41f8307
nick repasy <nrepasy@gmail.com>
2020-12-04 02:03:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,10 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
  data.each_with_object({}) do |(key, value), final_array|
+ binding.pry
   value.each do |inner_key, names|
 end
-  binding.pry
+ 
   
    final_array
  
