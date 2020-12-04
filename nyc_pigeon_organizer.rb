78ad643a659748314c3b66d1bc513a6c418a054e
nick repasy <nrepasy@gmail.com>
2020-12-04 02:10:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   value.each do |inner_key, names|
     names.each do |name|
       binding.pry
-      if !final_array[name]
+      if !final_array[name]'='
         final_array[name] = {}
         if !final_array[name][key] = {}
 end
